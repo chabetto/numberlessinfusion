71e6fb40ffffe00909d974a930c88fb2ad99dd6b
chabetto <zabetom@gmail.com>
2021-03-20 21:01:44 +0000

    added gen filling

--- script.js
+++ script.js
@@ -1,26 +1,115 @@
 /*
 to be done:
-
+game loool
 */
 
+// original player stats (do not save)
+let ogPlayer = {
+    times: {
+        Alpha: 5,
+    },
+    names: [
+        "Alpha",
+        "Beta",
+        "Gamma",
+        "Delta",
+        "AlphaBeta",
+        "BetaDelta",
+        "AlphaGamma",
+        "GammaDelta",
+    ],
+};
+
+// what will prob be the original save
 let player = {
-    NAMES: ["alpha", "beta", "gamma", "delta"],
-    INFUSIONS: ["AlphaBeta", "BetaGamma", "AlphaGamma", "GammaDelta"],
+    bars: {
+        Alpha: {
+            name: "Alpha",
+            time: 5,
+            vertical: true,
+            infusion: false,
+            percentage: 0,
+            unlocked: true,
+        },
+    },
+    NAMES: [
+        "Alpha",
+        "Beta",
+        "Gamma",
+        "Delta",
+        "AlphaBeta",
+        "BetaDelta",
+        "AlphaGamma",
+        "GammaDelta",
+    ],
+    TIMES: [5, 50, 500, 3600, 30, 30, 30, 30],
+    VERTICAL: [true, true, true, true, true, false, false, true],
+    INFUSION: [false, false, false, false, true, true, true, true],
+    PERCENTAGE: [0, 0, 0, 0, 0, 0, 0, 0],
+    UNLOCKED: [true, false, false, false, false, false, false, false],
     bought: [],
     permanent: [],
     unlockedGen: [true, false, false, false],
     unlockedInf: [false, false, false, false],
-    barWidth: {
-        alpha: 0,
-        beta: 0,
-        gamma: 0,
-        delta: 0,
-        AlphaBeta: 0,
-        BetaGamma: 0,
-        AlphaGamma: 0,
-        GammaDelta: 0,
-    },
+    update: 0.05,
+    stopTime: false,
 };
+
+class resource {
+    constructor(name, time, vertical, infusion, percentage, unlocked) {
+        this.name = name;
+        this.bar = document.querySelector(`#bar${name}`);
+        this.container = infusion
+            ? document.querySelector(`#infusion${name}`)
+            : document.querySelector(`#generator${name}`);
+        this.point = Boolean(percentage === 100);
+        this.vertical = vertical; // 1 means bar progresses vertically
+        this.time = time;
+        this.unlocked = unlocked;
+        this.percentage = percentage;
+        if (unlocked) {
+            this.unlock();
+        }
+        this.showPercentage();
+    }
+    unlock() {
+        showClass(this.name);
+        this.unlocked = true;
+    }
+    showPercentage() {
+        this.vertical
+            ? (this.bar.style.height = `${this.percentage}%`)
+            : (this.bar.style.width = `${this.percentage}%`);
+    }
+    updateBar() {
+        if (this.percentage < 100) {
+            this.percentage =
+                this.percentage + (player.update * 100) / this.time;
+            if (this.percentage >= 100) {
+                this.percentage = 100;
+                this.point = true;
+            }
+            this.showPercentage();
+        } else if (this.percentage === NaN) {
+            this.reset();
+        }
+    }
+}
+
+let resources = {};
+
+function createResources() {
+    for (i in player.NAMES) {
+        resources[player.NAMES[i]] = new resource(
+            player.NAMES[i],
+            player.TIMES[i],
+            player.VERTICAL[i],
+            player.INFUSION[i],
+            player.PERCENTAGE[i],
+            player.UNLOCKED[i]
+        );
+    }
+}
 
 // insert class name
 function hideClass(classStr) {
@@ -83,11 +172,30 @@ function buttonFunction(e) {
     let id = e.target.id;
     console.log(id);
     if (id.slice(id.length - 3) === "Tab") showTab(id);
-    else if (id.slice(id.length - 6) === "Unlock") unlockFunction(id);
+    else if (id.slice(0, 6) === "unlock") unlockFunction(id);
 }
 
-function unlockFunction(id) {}
+function unlockFunction(id) {
+    //console.log('hello')
+}
+
+function incrementBars() {
+    for (item in resources) {
+        if (resources[item].unlocked) {
+            resources[item].updateBar();
+        }
+    }
+}
+
+function startTime() {
+    let gameTime = window.setInterval(() => {
+        incrementBars();
+        if (player.stopTime) clearInterval(gameTime);
+    }, player.update * 1000);
+}
 
 window.onload = function () {
     addButtonListeners(false);
+    createResources();
+    startTime();
 };
